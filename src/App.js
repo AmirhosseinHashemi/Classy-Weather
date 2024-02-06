6e3b68dd67329ae25a127c990423886500ada6af
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-06 19:28:50 +0330

    🔨 fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React from "react";
 
+function convertToFlag(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+
+  return String.fromCodePoint(...codePoints);
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       location: "kashan",
+      displayLocation: "",
+      weather: {},
     };
+
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  async fetchWeather() {
+    try {
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      if (!geoData.results) throw new Error("Location not found");
+
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+
+      this.setState({
+        displayLocation: `${name} ${convertToFlag(country_code)}`,
+      });
+
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+
+      this.setState({ weather: weatherData.daily });
+    } catch (err) {
+      console.err(err);
+    }
   }
 
   render() {
@@ -26,7 +69,7 @@ class App extends React.Component {
           ></input>
         </div>
 
-        <button>Get Weather</button>
+        <button onClick={this.fetchWeather}>Get Weather</button>
       </div>
     );
   }
