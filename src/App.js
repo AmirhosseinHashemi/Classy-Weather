4501eb76f73cbb906cc7cb0866dfd92b950a172d
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-06 19:02:55 +0330

    🔨 create input and controll it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: "kashan",
+    };
+  }
+
   render() {
-    return <div>Hello React</div>;
+    return (
+      <div className="app">
+        <h1>Classy Weather</h1>
+
+        <div>
+          <input
+            type="text"
+            value={this.state.location}
+            onChange={(e) =>
+              this.setState({
+                location: e.target.value,
+              })
+            }
+          ></input>
+        </div>
+
+        <button>Get Weather</button>
+      </div>
+    );
   }
 }
 
